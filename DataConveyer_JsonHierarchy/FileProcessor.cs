1628df4079d31eb0c77f2c46fb7a4e2529550f6a
Wojtek Dziegielewski <wojtekd@mavidian.com>
2021-01-30 01:29:38 -0500

    Implement DataByYear to DataByType conversion.

--- DataConveyer_JsonHierarchy/FileProcessor.cs
+++ DataConveyer_JsonHierarchy/FileProcessor.cs
@@ -40,7 +40,6 @@ namespace DataConveyer_JsonHierarchy
             InputFileName = inFile,
             AllowOnTheFlyInputFields = true,  // TODO: (in Data Conveyer) consider UnboundJSON ignoring this setting like X12 
             AllowTransformToAlterFields = true,  //IMPORTANT! otherwise null items will be produced!
-            TransformerType =  TransformerType.Clusterbound,
             OutputDataKind = KindOfTextData.UnboundJSON,
             XmlJsonOutputSettings = "IndentChars|  ",  // pretty print
             OutputFileName = outLocation + Path.DirectorySeparatorChar + outFileName
@@ -50,12 +49,15 @@ namespace DataConveyer_JsonHierarchy
          {
             case ConversionMode.Flat_to_ByYear:
                config.ClusterMarker = (r, pr, i) => pr == null || (string)pr["State"] != (string)r["State"];  // cluster by state (intake records need to be sorted by state) - note casts to compare values, not references
+               config.TransformerType = TransformerType.Clusterbound;
                config.ClusterboundTransformer = ConvertFlatToByYear;
                break;
             case ConversionMode.ByYear_to_ByType:
-               config.ClusterboundTransformer = ConvertByYearToByType;
+               config.TransformerType = TransformerType.Recordbound;
+               config.RecordboundTransformer = ConvertByYearToByType;
                break;
             case ConversionMode.ByType_to_Flat:
+               config.TransformerType = TransformerType.Clusterbound;
                config.ClusterboundTransformer = ConvertByTypeToFlat;
                break;
          }
@@ -78,7 +80,7 @@ namespace DataConveyer_JsonHierarchy
 
 
       private ICluster ConvertFlatToByYear(ICluster inClstr)
-      {
+      {  //each cluster contains flat records for a state; all records in a cluster get collapsed into a single record
          var outClstr = inClstr.GetEmptyClone();
          var outRec = inClstr[0].GetEmptyClone();  // a single output record for the state
          outRec.AddItem("State", inClstr[0]["State"]);
@@ -95,10 +97,30 @@ namespace DataConveyer_JsonHierarchy
          return outClstr;
       }
 
-      private ICluster ConvertByYearToByType(ICluster inClstr)
-      {
-         throw new NotImplementedException();
+      private IRecord ConvertByYearToByType(IRecord inRec)
+      {  //here, there is a 1 to 1 relationship between input and output records (a record per state), so RecordBound transformes is used
+         static string Get2ndToLastSegment(string key) => key.Split('.').Reverse().Skip(1).First();
+         var outRec = inRec.GetEmptyClone();
+         outRec.AddItem("State", inRec["State"]);
+         foreach (var itm in inRec.Items.Where(i => i.Key.EndsWith("Population")))
+         {  //note that items do not to be sorted by year here (UnboundJSON output provider will group them appropriately)
+            var year = Get2ndToLastSegment(itm.Key);
+            outRec.AddItem($"DataByType[0].Population.{year}", itm.Value);
+         }
+         foreach (var itm in inRec.Items.Where(i => i.Key.EndsWith("Drivers")))
+         {  //like above, no sort by year is needed here
+            var year = Get2ndToLastSegment(itm.Key);
+            outRec.AddItem($"DataByType[0].Drivers.{year}", itm.Value);
+
+         }
+         foreach (var itm in inRec.Items.Where(i => i.Key.EndsWith("Vehicles")))
+         {  //neither is sort by year needed here
+            var year = Get2ndToLastSegment(itm.Key);
+            outRec.AddItem($"DataByType[0].Vehicles.{year}", itm.Value);
+         }
+         return outRec;
       }
+
 
       private ICluster ConvertByTypeToFlat(ICluster inClstr)
       {
@@ -122,9 +144,9 @@ namespace DataConveyer_JsonHierarchy
             case "_flatdata.json":
                return (ConversionMode.Flat_to_ByYear, OutName: Regex.Replace(inputName, _trailerPattern, "_DataByYear.json"));
             case "_databyyear.json":
-               return (ConversionMode.Flat_to_ByYear, OutName: Regex.Replace(inputName, _trailerPattern, "_DataByType.json"));
+               return (ConversionMode.ByYear_to_ByType, OutName: Regex.Replace(inputName, _trailerPattern, "_DataByType.json"));
             case "_databytype.json":
-               return (ConversionMode.Flat_to_ByYear, OutName: Regex.Replace(inputName, _trailerPattern, "_FlatData.json"));
+               return (ConversionMode.ByType_to_Flat, OutName: Regex.Replace(inputName, _trailerPattern, "_FlatData.json"));
             default:
                return (ConversionMode.Undetermined, OutName: null);
          }
